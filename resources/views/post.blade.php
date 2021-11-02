7e792f711e8da83d9e4539f45c4621f41012e776
Abdi <m.abdi@schubwerk.de>
2021-11-02 15:05:41 +0100

    initial commit 2

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <article>
+
+        <h1><?= $post->title ?></h1>
+
+    </article>
+    <div>
+        <?= $post->body; ?>
+    </div>
+</body>
